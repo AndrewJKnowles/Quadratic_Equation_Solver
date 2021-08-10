6cc4fd539ee59dede005ad19f8ce4655d5e3639c
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2021-08-10 13:38:59 +0100

    Update Quadraticprogram.m

--- Quadraticprogram.m
+++ Quadraticprogram.m
@@ -1,12 +1,18 @@
 clc;
+
 prompt='What is the value of a?  ';
 a=input(prompt);
+
 prompt='What is the value of b?  ';
 b=input(prompt);
+
 prompt='What is the value of c?  ';
 c=input(prompt);
+
 prompt='enter lgd ';
+
 d=((b^2)-(4*a*c));
+
 if d<0
     disp('This quadratic has no real roots.');
     x=[-200:0.0001:200];
@@ -15,6 +21,7 @@ if d<0
     title('Graph of ax^2+bx+c');
     xlabel('x axis');
     ylabel('y axis');
+    
 else if d==0
         x=-b/(2*a);
         disp(['This quadratic has equal roots at: ',num2str(x),'.']);
@@ -24,11 +31,13 @@ else if d==0
         title('Graph of ax^2+bx+c');
         xlabel('x axis');
         ylabel('y axis');
+        
     else
         x1=((-b)+(d^0.5))/(2*a);
         x2=((-b)-(d^0.5))/(2*a);
         y=['The quadratic roots are x1= ',num2str(x1),' and x2= ',num2str(x2)];
         disp(y);
+        
         if x1>x2
             x=[x2:0.0001:x1];
             y=(a*(x.^2))+(b*x)+c;
@@ -36,6 +45,7 @@ else if d==0
             title('Graph of ax^2+bx+c');
             xlabel('x axis');
             ylabel('y axis');
+            
         else
             x=[x1:0.0001:x2];
             y=(a*(x.^2))+(b*x)+c;
